b2b87710ddbc492372ac137b75e283caa3c4f840
lukoh.nam <lukoh.nam@gmail.com>
2016-06-14 11:24:25 +0900

    RecyclerFragment has been improved to boost performance.

--- app/src/main/java/com/goforer/fyber_challenge_android/ui/fragment/OfferListFragment.java
+++ app/src/main/java/com/goforer/fyber_challenge_android/ui/fragment/OfferListFragment.java
@@ -153,7 +153,6 @@ public class OfferListFragment extends RecyclerFragment<Offers> {
         if (mCurrentPage > mTotalPageNum && mTotalPageNum > 1) {
             doneRefreshing();
             mAdapter.setReachedToLastPage(true);
-            showToastMessage(getString(R.string.toast_last_page));
             return;
         }
 
